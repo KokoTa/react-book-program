8c146f3c9ddc243158285d87b3d442133696d539
KokoTa <584847514@qq.com>
2018-08-11 15:38:56 +0800

    add, PropTypes/DefaultProps/LifeLoop/Charles/react-transition-group

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,17 +1,48 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class TodoItem extends Component {
   
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShow: true
+    }
+  }
+
+  // 接受参数前(生成组件时不触发)，用于避免重复渲染
+  componentWillReceiveProps = (prevProps, prevState) => {
+    if (this.props.item === prevProps.item) return false;
+    console.log('子组件接受参数前触发', prevProps, prevState);
+  }
+  // 销毁后触发
+  componentWillUnmount = () => {
+    console.log('子组件销毁后触发');
+  }
+  
+  
   render() {
-    const { item, index, deleteItem } = this.props;
+    const { item, index, deleteItem, prefix } = this.props;
 
     return (
       <li
         onClick={() => deleteItem(index)}
-        dangerouslySetInnerHTML={{__html: item}}>
+        dangerouslySetInnerHTML={{__html: (prefix + ' - ' + item)}}>
       </li>
     )
   }
+}
+
+// 类型检测
+TodoItem.propTypes = {
+  item: PropTypes.string,
+  deleteItem: PropTypes.func,
+  index: PropTypes.number,
+  prefix: PropTypes.string.isRequired,
+}
+
+TodoItem.defaultProps = {
+  prefix: 'prefix'
 }
 
 export default TodoItem;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,6 @@
 import React, { Component, Fragment } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import axios from 'axios';
 import TodoItem from './TodoItem';
 import './TodoList.css';
 
@@ -8,7 +10,7 @@ class TodoList extends Component {
     super(props);
     this.state = {
       inputValue: '',
-      list: []
+      list: ['a', 'b', 'c', 'd', 'e', 'f']
     };
   }
   
@@ -34,27 +36,74 @@ class TodoList extends Component {
       const list = [...prevState.list];
       list.splice(index, 1);
       return { list };
-    });
+    }, () => console.log('删除完毕'));
   }
 
   // 输出项
   getItems = () => {
     return this.state.list.map((item, index) => {
       // 传递方法时记得绑定 this，否则 this 指向子组件
-      return <TodoItem item={item} deleteItem={this.deleteItem} index={index} key={index}></TodoItem>
+      return (
+        // 注意 key 值不要使用 index 否则删除时会导致所有项都重新渲染，so bad= =
+        <CSSTransition
+          timeout={1000}
+          classNames='fade'
+          appear={true}
+          key={item}>
+          <TodoItem item={item} deleteItem={this.deleteItem} index={index}></TodoItem>
+        </CSSTransition>
+      )
     });
+  }
+
+  // 挂载前
+  componentWillMount = () => {
+    console.log('挂载前触发');
+  }
+  // 挂载后(建议在此声明周期进行数据请求)
+  componentDidMount = () => {
+    axios.get('/api/getlist')
+      .then((res) => {
+        if (res.status === 200) {
+          this.setState({
+            list: [...res.data]
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+
+    console.log('挂载后触发');
+  } 
+  // 更新前进行确认
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+     console.log('更新前确认触发', nextProps, nextState, nextContext);
+     // 为 true，更新许可
+     return true;
+  }
+  // 更新前
+  componentWillUpdate = (nextProps, nextState) => {
+    console.log('更新前触发', nextProps, nextState);
+  }
+  // 更新后
+  componentDidUpdate = (prevProps, prevState) => {
+    console.log('更新后触发', prevProps, prevState);
   }
   
   render() {
+    console.log('渲染触发');
     return (
       <Fragment>
         <div className='wrap'>
           <label htmlFor='insert'>输入事项：</label>
-          <input type='text' id='insert' className='input' value={this.state.inputValue} onChange={this.handleChange}/>
+          <input type='text' id='insert' className='input' value={this.state.inputValue} onChange={this.handleChange} ref={(input) => this.input = input}/>
           <button className='btn' onClick={this.handleClick}>提交</button>
         </div>
         <ul>
-          {this.getItems()}
+          <TransitionGroup component={null}>
+            {this.getItems()}
+          </TransitionGroup>
         </ul>
       </Fragment>
     )
